3cf0e8172ec76bbf6fcc945004a0b3ed6703acb1
Aramis Doejo <adoejo.salud2010@gmail.com>
2024-04-02 22:11:55 -0400

    add .

---
+++ app/src/main/java/com/adoejosld/basiclayoutscodelab/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.adoejosld.basiclayoutscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adoejosld.basiclayoutscodelab.ui.theme.BasicLayoutsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicLayoutsCodelabTheme {
+                SearchBar(modifier = Modifier)
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(id = R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(all = 8.dp)
+            .heightIn(56.dp)
+    )
+}
+
+@Composable
+@Preview(showBackground = true, widthDp = 320)
+fun SearchBarPreview() {
+    BasicLayoutsCodelabTheme {
+        Surface {
+            SearchBar(modifier = Modifier)
+        }
+    }
+}
